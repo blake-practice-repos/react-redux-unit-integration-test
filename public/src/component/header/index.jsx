1ee3715874862a22fd5e8a37f1402abb4560ab7c
blake-swl <seulwoolee93@gmail.com>
2019-07-14 22:12:48 -0700

    Initial commit of my comps without tests

---
+++ public/src/component/header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.scss';
+
+const Header = props => {
+  return (
+    <header>
+      <div className="wrap">
+        <div className="logo">
+          Logo
+        </div>
+      </div>
+    </header>
+  )
+}
+export default Header;

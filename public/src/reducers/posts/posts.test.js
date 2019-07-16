c74368efbeb22707a77ec50e6bccb1ec23acfa1f
blake-swl <seulwoolee93@gmail.com>
2019-07-15 20:17:18 -0700

    [add] posts reducer

---
+++ public/src/reducers/posts/posts.test.js
@@ -0,0 +1,20 @@
+import { types } from '../../actions/types';
+import postsReducer from './reducer';
+
+describe('Posts Reducer', () => {
+
+  it('should return default state', () => {
+    const newState = postsReducer(undefined, {});
+    expect(newState).toEqual([]);
+  });
+
+  it('should return new state if receiving type', () => {
+    const posts = [{ title: 'Test1' }, { title: 'Test2' }, { title: 'Test3' }];
+    const newState = postsReducer(undefined, {
+      type: types.GET_POSTS,
+      payload: posts
+    })
+    expect(newState).toEqual(posts)
+  })
+
+})
